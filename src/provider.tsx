d141bf2c121c21bd6128fa3cb333e9e988545b08
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-11-15 13:06:29 +0700

    fising all

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryConfig } from "./libs/react-query";
-export default function Provider({ children }: { children: any }) {
+export default function Provider({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(
     () =>
       new QueryClient({
